ec308e2bec1e835925cdd1ce11610f194a88387f
coderdaudat <coderdaudat@gmail.com>
2018-12-20 21:25:49 +0700

    Add log

--- src/main/java/com/beemob/becaslt/demotest/DemoApplication.java
+++ src/main/java/com/beemob/becaslt/demotest/DemoApplication.java
@@ -1,13 +1,19 @@
 package com.beemob.becaslt.demotest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	private final static Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		logger.info("Start DemoApplication");
+		logger.error("Start Error DemoApplication");
 	}
 
 }
