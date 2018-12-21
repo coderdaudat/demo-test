38fa05e5100f13dcd604ab220ac9d08b3616b148
coderdaudat <coderdaudat@gmail.com>
2018-12-21 10:34:37 +0700

    Add controller

---
+++ src/main/java/com/beemob/becaslt/demotest/TestController.java
@@ -0,0 +1,26 @@
+package com.beemob.becaslt.demotest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by truongnguyen on 12/21/18.
+ */
+@RestController
+public class TestController {
+
+    private final static Logger logger = LoggerFactory.getLogger(TestController.class);
+
+    @RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json")
+    public ResponseEntity<?> createUser(@RequestParam("name") String userDto) {
+        logger.info("NAME: " + userDto);
+        return new ResponseEntity(userDto, HttpStatus.OK);
+    }
+
+}
